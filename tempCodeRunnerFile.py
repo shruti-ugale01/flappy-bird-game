abadf339aff987a0f8555e2f5467102318084f96
shruti-ugale01 <shrutiugale10808@gmail.com>
2024-11-17 00:53:54 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+import pygame as pg
+
+
+class Bird(pg.sprite.Sprite):
+    def __init__(self,scale_factor):
+        super(Bird,self).__init__()
+        self.img_list=[pg.transform.scale_by(pg.image.load("assets/birdup.png").convert_alpha(),scale_factor),
+                        pg.transform.scale_by(pg.image.load("assets/birddown.png").convert_alpha(),scale_factor)]
+
+        self.image_index=0
+        self.image=self.img_list[self.image_index]
+        self.rect=self.image.get_rect(center=(100,100))
+        self.y_velocity=0
+        self.gravity=10
+        self.flap_speed=250
+        self.anim_cou
